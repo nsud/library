971edb65c9389c74c155f065292526c38947b8ec
User <natali29915021@mail.ru>
2020-08-07 17:33:11 +0300

    +

---
+++ p_library/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from p_library.models import Book, Author, Publisher
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+
+    list_display = ('title', 'author_full_name',)
+    fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'price', 'publisher')
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+    pass
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    pass
---
+++ p_library/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Publisher(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    copy_count = models.CharField(max_length=100, default=1)
+    price = models.DecimalField(decimal_places=2, max_digits=10000, default=0.00)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, blank=True, null=True,)
+
+    def __str__(self):
+        return self.title
